ecebdfb867be0c83dfbaab5f299b284ae8779ec1
mehmet <turmehmetkorkmaz@gmail.com>
2021-02-27 14:28:12 +0100

    Console tutorials

---
+++ Basics/JS-1. PART/Console/app.js
@@ -0,0 +1,8 @@
+console.log("buraya ne yazarsan console'da o gorunur")
+console.log("merhaba");
+var a=10;
+console.log(a)
+console.log(typeof a);
+console.log("console; web gelistirici aracininin icerisindeki bir bolumdur.");
+console.warn("bu bir uyari yazisidir");
+console.error("bu sayfa bulunamadi");
---
+++ Basics/JS-1. PART/Veri tipleri/app.js
@@ -0,0 +1,7 @@
+// Degisken olusturma
+
+var a = 20;
+var b= 10;
+var c=40;
+
+console.log(a,b,c);
