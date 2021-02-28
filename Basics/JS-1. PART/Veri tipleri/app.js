538ca7b1bfd1508fcaa1bfc4b1f8087348996818
mehmet <turmehmetkorkmaz@gmail.com>
2021-02-28 11:19:48 +0100

    let, const

--- Basics/JS-1. PART/Veri tipleri/app.js
+++ Basics/JS-1. PART/Veri tipleri/app.js
@@ -1,7 +1,60 @@
 // Degisken olusturma
 
-var a = 20;
-var b= 10;
-var c=40;
+// var a=20;
+// var b=10;
+// var c=40;
 
-console.log(a,b,c);
+// console.log(a,b,c);
+
+//veri tipleri
+
+//1. primitive veri tipi
+
+//number, string, boolean, null
+// console.log(typeof 1)
+
+// var elma="kirmizi";
+// console.log(elma);
+
+// var a=3
+// console.log(a);
+
+// Boolean
+// var a=false
+// console.log(typeof a);
+
+
+
+// var a=null;
+// //null; a nin bir deger tasimadigini, bos oldugunu soyler
+
+// console.log(typeof a);
+
+// //undefined; hic deger verilmemis bir elemani belirtir,
+// var a;
+
+// console.log(a);
+
+//2. reference veri tipi
+
+// var numbers=[1,2,3,4,5];
+// console.log(numbers);
+// console.log(typeof numbers)
+
+// var person ={
+//     name:"Mehmet",
+//     age:25
+// }
+// console.log(person);
+// console.log(typeof person)
+
+// var date=new Date();
+// console.log(date);
+// console.log(typeof date)
+
+var merhaba=fucntion(){
+    console.log("merhaba");
+}
+
+
+console.log("merhaba");
---
+++ Basics/JS-1. PART/var, let, cons.html/app.js
@@ -0,0 +1,55 @@
+
+//let
+// var name="Mustafa";
+// var name="Mehmet"
+// console.log(name);
+
+//let 
+// let name="Mustafa Murat"
+// console.log(name);
+// name="Mustafa";
+// console.log(name);
+
+//let ile  var arasinda ki fark nedir ? 
+//asagida goruldugu sekilde iki kere tanimlayamam
+
+// let name="Mehmet";
+// let name="korkmaz";
+// console.log(name);  (hata verdi)
+
+// // fakat asagida goruldugu sekilde var ile iki kere tanimlayabilirim
+// var name="Mehmet";
+// var name="korkmaz";
+// console.log(name);
+
+
+// let a,b;
+// a=10;
+// b=20;
+// console.log(a+b);
+
+//const (sabit) constant
+
+// let ile const arasindaki fark
+//asagida goruldugu gibi iki kere deger veremiyorum
+
+// const name= "mustafa murat";
+// console.log(name);
+// name="mustafa";
+// console.log(name);  (hata verdi)
+
+//ama let ile bunu yapabiliyorum
+
+// let name="Mustafa Murat"
+// console.log(name);
+// name="Mustafa";
+// console.log(name);
+
+const a=[1,2,3];
+console.log(a);
+a.push(6);
+console.log(a);
+
+
+
+
