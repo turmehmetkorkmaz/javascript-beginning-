594426b67f935a5117944a7416adfd5aa99e563d
mehmet <turmehmetkorkmaz@gmail.com>
2021-03-12 11:50:51 +0100

    temel js devam

---
+++ Basics/JS-1. PART/Switch case Yapilari/app.js
@@ -0,0 +1,15 @@
+const progress = 3   ;
+
+switch(progress) {
+    case 1:
+        console.log("islem 1");
+        break;
+    case 2:
+        console.log("islem 2");
+        break;
+    default:
+        console.log("gecersiz sayi");
+}
+
+
+
---
+++ Basics/JS-1. PART/windows/app.js
@@ -0,0 +1,37 @@
+// alert("Merhaba");
+
+// console.log(this.alert("Merhaba"));
+// console.log(window.alrt("Merhaba"));
+
+// const cevap = confirm("Emin misiniz?")
+
+// if(cevap){
+//     console.log("silin gitsin");
+// }
+// else{
+//     console.log("silmeyin");
+// }
+
+
+// ya da asagidaki gibi yazilabilir
+
+
+// if(confirm("Emin misiniz?")){
+//     console.log("silin gitsin");
+// }
+// else{
+//     console.log("silmeyin");
+// }
+// input alarak basarili giris yaptirma 
+
+const cevap = prompt("2+3");
+if (cevap == 55){
+
+    console.log("basarili");
+}
+
+else{
+console.log("basarisiz");
+}
+
+
