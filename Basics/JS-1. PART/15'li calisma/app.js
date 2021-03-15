601278c57d8b287535b2e974b270426dc79c5be4
mehmet <turmehmetkorkmaz@gmail.com>
2021-03-15 16:57:01 +0100

    js.beginning

--- Basics/JS-1. PART/15'li calisma/app.js
+++ Basics/JS-1. PART/15'li calisma/app.js
@@ -1,8 +1,101 @@
-var person = {
-    firstName: "John",
-    lastName : "Doe",
-    id       : 5566,
-    fullName : function() {
-      return this.firstName + " " + this.lastName;
-    }
-  };
+// let example1=["ibo","neco"]
+// console.log(example1);
+// example1.splice(1,0,"hirto")
+// console.log(example1);
+// example1.indexOf("ibo")
+
+
+// let array3 = ["product1", "product2", "product3", "product4", "product5"]
+// let a= array3.indexOf("product6")
+// if (a)
+// {
+// alert("product6 yok");
+// }
+
+
+// let array1 = ["mehmet", 2529, "Kayseri", true]
+
+// let array2 = [4, 5, 78, 21, 43]
+
+// let array3 = ["product1", "product2", "product3", "product4", "product5", "product6", "product7"]
+
+// let array4 = array3.concat(array1)
+// console.log(array4);
+
+
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+
+// console.log(array3);
+// // expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+
+// let necoArr=["memo", "adamin","dibi"]
+
+// let array2 = [4, 5, 78, 21, 43]
+
+// let newArr=array2.map(item => item*2 )
+// console.log(newArr);
+
+
+let array4 = [
+  {
+      id: 1,
+      title: "iPhone 11 Pro 256 Gb-NightGreen",
+      imgUrl: "https://www.telekom.de/resources/images/480496/apple-iphone-11-violett-vorne.png",
+      comments:
+          [
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id."
+          ]
+  },
+  {
+      id: 2,
+      title: "iPhone 12 Pro Max",
+      imgUrl: "https://mobile.1und1.de/static/6c2/c154a25752aa7586fff558f30af3e/webapp/5bb53d1b9ce378f036501f4a9aafb35e-iPhone-12pro-max-couple-blue.png",
+      comments:
+          [
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+          ]
+  },
+  {
+      id: 3,
+      title: "Samsung S10",
+      imgUrl: "https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_74715590/fee_786_587_png",
+      comments:
+          [
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id."
+          ]
+  },
+  {
+      id: 4,
+      title: "Xiaomi Mi 11",
+      imgUrl: "https://assets.mobilfunk.check24.de/filestore/tariffaddon_hardware_images/xiaomi-mi-11-grey_double_zoom.png?v=1",
+      comments:
+          [
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, id.",
+
+          ]
+  },
+]
+
+let myArr = array4.map((x)=>{
+
+  return {ibo
+}})
---
+++ Basics/JS-1. PART/math objesi/Scope kavrami/app.js
@@ -0,0 +1,6 @@
+//scope kavarami
+
+// 1. global scope
+// 2. functional scope
+// 3. block scope 
+
